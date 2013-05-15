957400e88dd1cf1a808e1f65d93d7fcfc874a162
Albert Wang <hwwang@tea.ntue.edu.tw>
2013-05-15 22:38:10 +0800

    P14 OK touch for Tim

--- Assets/Components/page14/script/p14_FerrisWheel.cs
+++ Assets/Components/page14/script/p14_FerrisWheel.cs
@@ -5,13 +5,19 @@ public class p14_FerrisWheel : MonoBehaviour {
     public float m_fdegree; // 目前轉動的角度
     public GameObject[]  m_CarObject = new GameObject[8];
     public p14_Carriages[] m_CarScript = new p14_Carriages[8];
-    public Vector2 tp;
+    public Vector2 m_PrevPos;
+    public Vector2 m_CurrPos;
+    public Vector2 m_TPDelta;
     public Vector2 m_FWCenter;
     public Ray touchRay;
+    public bool m_bMovinWheel;
+
+    public float m_t;
 
 
 	// Use this for initialization
 	void Start () {
+        m_bMovinWheel = false;
         m_fdegree = 0; // 預設為0度	
         m_CarObject[0] = GameObject.Find("aonp14_i_fun11");
         m_CarObject[1] = GameObject.Find("aonp14_i_fun12");
@@ -31,54 +37,50 @@ public class p14_FerrisWheel : MonoBehaviour {
 	void Update () {
         if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer || Application.platform == RuntimePlatform.MetroPlayerARM)
         {
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                // 根據拖曳來計算轉動的角度
-                // Get movement of the finger since last frame
-                touchRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            touchRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began ) {
+                if (touchRay.origin.x >= -4.8f && touchRay.origin.x <= 0.6f && touchRay.origin.y >= -1.5f && touchRay.origin.y <= 3.5f)
+                { // 起點落在摩天輪的範圍內
+                    m_PrevPos.x = touchRay.origin.x;
+                    m_PrevPos.y = touchRay.origin.y;
+                    m_bMovinWheel = true;
+                }
+            }
+            else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
+                // 根據拖曳的順逆時方向來設定轉動的方向, 根據移動的差距來計算轉動的角度
                 if (touchRay.origin.x >= -4.8f && touchRay.origin.x <= 0.6f && touchRay.origin.y >= -1.5f && touchRay.origin.y <= 3.5f)
                 {
-                    // 分成四個象限來處理
-                    tp = Input.GetTouch(0).deltaPosition;
-                    if (touchRay.origin.x >= m_FWCenter.x) {
-                        if ( touchRay.origin.y >= m_FWCenter.y) { // I 象限
-                            if (tp.x < 0) {
-                                m_fdegree = -(tp.x * tp.x + tp.y * tp.y) * 0.5f;
-                            }
-                            else {
-                                m_fdegree = (tp.x * tp.x + tp.y * tp.y) * 0.5f;
-                            }
-                        }
-                        else { // IV 象限
-
-                        }
-                    }
-                    else {
-                        if ( touchRay.origin.y >= m_FWCenter.y ) { // II 象限
-
-                        }
-                        else  { // III 象限
-
-                        }
-                    } 
-
-                    // 根據點的位置 與 tp 的內容來設定轉動的方向
-
-//                    float t = Time.deltaTime;
+                    m_CurrPos.x = touchRay.origin.x;
+                    m_CurrPos.y = touchRay.origin.y;
+                    float px, py, nx, ny, t;
+                    px = m_PrevPos.x - m_FWCenter.x; // 摩天輪中心指向前一個 TP 的向量
+                    py = m_PrevPos.y - m_FWCenter.y;
+                    nx = m_CurrPos.x - m_FWCenter.x; // 摩天輪中心指向目前 TP 的向量
+                    ny = m_CurrPos.y - m_FWCenter.y;
+                    m_t = px * ny - py * nx; // (px,py) 與 (nx, ny) 的外積, > 0 代表逆時針, < 0 代表順時針
+                    m_TPDelta = Input.GetTouch(0).deltaPosition;
+                    if (m_t >= 0) m_fdegree = -m_TPDelta.SqrMagnitude()*1.5f;
+                    else m_fdegree = m_TPDelta.SqrMagnitude() * 1.5f;
                     transform.Rotate(0, m_fdegree, 0);
                     for (int i = 0; i < 8; i++) m_CarScript[i].SetDegree(m_fdegree); // 有呼叫才需要轉動一次, 採用一步到位的方式執行?
                     m_fdegree = 0;
-
+                    m_PrevPos = m_CurrPos;
+                }
+                else {
+                    if (m_bMovinWheel) m_bMovinWheel = false;
                 }
             }
+            else {
+                if( m_bMovinWheel ) m_bMovinWheel = false;
+            }
         }
-
 	}
 
     void OnGUI()
     {
-        GUI.Label(new Rect(Screen.currentResolution.width / 2, Screen.currentResolution.height / 2, 200, 50), tp.ToString());
-        GUI.Label(new Rect(Screen.currentResolution.width / 2, Screen.currentResolution.height / 2-300, 200, 50), touchRay.ToString());
+        //GUI.Label(new Rect(Screen.currentResolution.width / 2+150, Screen.currentResolution.height / 2-300, 200, 50), m_CurrPos.ToString());
+        //GUI.Label(new Rect(Screen.currentResolution.width / 2 + 150, Screen.currentResolution.height / 2 -250, 200, 50), m_PrevPos.ToString());
+        //GUI.Label(new Rect(Screen.currentResolution.width / 2 + 150, Screen.currentResolution.height / 2 -200, 200, 50), m_t.ToString());
     }
 }
 
